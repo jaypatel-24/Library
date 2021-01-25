97c6e3937012bb4b6a79359fc32cefe70c4a47ee
Jay Patel <friend.jay24@gmail.com>
2021-01-25 17:12:50 +0530

    the final commit

--- library.js
+++ library.js
@@ -1,34 +1,32 @@
 
-//let addBook = document.querySelector('#addBook');
-//let showBooks = document.querySelector('#showBooks');
-
 let library =[];
 
 
-function Books(bookTitle,bookAuthor,nop,read) {
+function Books(bookTitle,bookAuthor,noOfPages,readStatus) {
 	this.bookTitle = bookTitle;
 	this.bookAuthor = bookAuthor;
-	this.nop = nop;
-	this.read=read;
+	this.noOfPages = noOfPages;
+	this.readStatus = readStatus;
 }
 
 
-function showPerticularBook({bookTitle,bookAuthor,nop,read},index) {
+function showPerticularBook({bookTitle,bookAuthor,noOfPages,readStatus},index) {
 	var book = `<tr><td>`+ bookTitle + `</td><td>` + bookAuthor + `</td><td>`
-		+ nop +`</td><td> <input type="button" id="deleteButton" value="del" onclick="deleteFunction(${index})"></td><td>` + read + `</td></tr>`;
-	document.getElementById("bookMenu").innerHTML += book;
+		+ noOfPages +`</td><td> <input type="button" id="deleteButton" value="del" onclick="deleteFunction(${index})"></td><td>` + readStatus + 
+		`</td><td> <input type="button" id="changeReadStatus" value="Change Read Status" onclick="changeReadStatusOfBook(${index})"></td></tr>`;
+	document.getElementById("listOfBooks").innerHTML += book;
 }
 
 function addNewBook(){
 
 	var local_bookTitle =  document.getElementById("bookTitle_id").value;
 	var local_bookAuthor = document.getElementById("bookAuthor_id").value;
-	var local_nop =  document.getElementById("nop_id").value;
-	var local_readStatus=false;
+	var local_noOfPages =  document.getElementById("noOfPages_id").value;
+	var local_readStatus = false;
 	if(document.getElementById("yes").checked == true)
 		local_readStatus=true;
 
-	let newBook = new Books(local_bookTitle,local_bookAuthor,local_nop,local_readStatus);
+	let newBook = new Books(local_bookTitle, local_bookAuthor, local_noOfPages, local_readStatus);
 	library.push(newBook);
 
 	showPerticularBook(newBook, library.length-1);
@@ -37,18 +35,27 @@ function addNewBook(){
 
 function showAllBooks(){
 	for(let i=0;i<library.length;i++) {
-		showPerticularBook(library[i],i);
+		showPerticularBook(library[i], i);
 	}
 }
 
 function deleteFunction(index) {
-	console.log(index);
 	library.splice(index,1);
-	document.getElementById("bookMenu").innerHTML ="";
+	document.getElementById("listOfBooks").innerHTML ="";
 	showAllBooks();
 	document.getElementById("totalBooks").innerHTML = `Total Books :  ${library.length}`;
 
 }
 
-document.querySelector('#addBook').addEventListener("click", addNewBook);
-//document.querySelector('#deleteBook').addEventListener("click", deletePerticularBook);
+function changeReadStatusOfBook(index) {
+	if(library[index].readStatus == true){
+		library[index].readStatus = false; 
+	} else {
+		library[index].readStatus = true; 
+	}
+
+	document.getElementById("listOfBooks").innerHTML ="";
+	showAllBooks();
+}
+
+document.querySelector('#addNewBook').addEventListener("click", addNewBook);
